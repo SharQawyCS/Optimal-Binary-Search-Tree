73bf05980680ebf5e161f3c9a8c87b90fdceb8f4
sharqawycs <sharqawycs@gmail.com>
2024-12-05 20:57:20 +0200

    enhance Utils.h and TreeNode.h documentation ✨

--- TreeNode.h
+++ TreeNode.h
@@ -3,11 +3,23 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @class TreeNode
+ * Represents a node in a binary tree. Each node stores a key and pointers to its left and right child nodes.
+ */
 class TreeNode
 {
 public:
-  std::string key;
-  TreeNode *left, *right;
+  std::string key; ///< The value or label of the node.
+  TreeNode *left;  ///< Pointer to the left child node.
+  TreeNode *right; ///< Pointer to the right child node.
 
+  /**
+   * @brief Constructor for the TreeNode class.
+   *
+   * Initializes the node with a given key and sets its child pointers to null.
+   *
+   * @param key The value or label to be assigned to the node.
+   */
   TreeNode(const std::string &key) : key(key), left(nullptr), right(nullptr) {}
 };
--- Utils.h
+++ Utils.h
@@ -1,5 +1,6 @@
 /**
- * This Class for utilities
+ * This class contains utility functions that are commonly used across the application.
+ * It includes functions for creating 2D vectors and displaying them.
  */
 
 #pragma once
@@ -9,31 +10,51 @@
 #include <string>
 #include "Vector.h"
 
-// TODO: A function to get the input from the user (cin not enough becuase we need better data validation )
-
+// TODO: A function to get the input from the user (cin not enough because we need better data validation)
 namespace Utils
 {
-  // A function to return a 2D Vector with specified sizes
+  /**
+   * @brief Creates a 2D vector with specified dimensions.
+   *
+   * This function creates and returns a 2D vector of type T with the specified number of rows and columns.
+   *
+   * @tparam T The type of elements stored in the vector.
+   * @param rows The number of rows in the 2D vector.
+   * @param cols The number of columns in the 2D vector.
+   * @return A 2D vector of size [rows][cols].
+   */
   template <typename T>
   Vector<Vector<T>> create2D(size_t rows, size_t cols)
   {
-    Vector<Vector<T>> result(rows);
+    Vector<Vector<T>> result(rows); // Create outer vector with 'rows' number of elements
     for (size_t i = 0; i < rows; ++i)
     {
-      result[i].resize(cols);
+      result[i].resize(cols); // Resize each inner vector to have 'cols' elements
     }
     return result;
   }
 
+  /**
+   * @brief Displays a 2D vector in a readable format.
+   *
+   * This function displays a 2D vector, with values printed in tabular format.
+   * Values that represent invalid data (where i - j > 0) are displayed as a dash ('-').
+   *
+   * @tparam T The type of elements stored in the vector.
+   * @param vec The 2D vector to be displayed.
+   */
   template <typename T>
   static void displayTwoDVec(const Vector<Vector<T>> &vec)
   {
-    int n = vec.size() - 2;
+    int n = vec.size() - 2; // Assuming 2 extra rows/columns for padding or special use
 
+    // Loop through rows
     for (int i = 1; i <= n; i++)
     {
+      // Loop through columns
       for (int j = 1; j <= n; j++)
       {
+        // Display a dash for invalid data or the actual value if valid
         if (i - j > 0)
           std::cout << "-\t\t";
         else
@@ -43,7 +64,7 @@ namespace Utils
     }
   }
 
-  // TODO: these two functions need to be impelemented and tested well
+  // TODO: these two functions need to be implemented and tested well
   /*
     void getDataFromUser(Vector<std::string> &DataLables, int &N, Vector<float> &P, Vector<float> &Q)
     {
@@ -72,7 +93,7 @@ namespace Utils
         Q[i] = in;
       }
 
-      // TODO: Get lables code should be here..
+      // TODO: Get labels code should be here..
       DataLables = Vector<std::string>(N);
 
       for (int i = 0; i < N; i++)
@@ -80,8 +101,8 @@ namespace Utils
         DataLables[i] = "Node " + std::to_string(i + 1);
       }
 
-      // std::cout << "Enter lables: ";
-      // // get lables with spaces and without std::cin >>
+      // std::cout << "Enter labels: ";
+      // // Get labels with spaces and without std::cin >>
       // std::cin.ignore();
       // for (size_t i = 0; i < N; i++)
       // {
@@ -104,5 +125,5 @@ namespace Utils
         DataLables[i] = "Node " + std::to_string(i + 1);
       }
     }
-    */
+  */
 }
